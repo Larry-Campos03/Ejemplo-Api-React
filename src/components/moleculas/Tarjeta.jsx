e9044856f4b3d4e4307c83a1907347543a1547b6
Larry Campos <larryisaza70@gmail.com>
2023-07-17 21:18:35 -0500

    Intentando guardar los datos

--- src/components/moleculas/Tarjeta.jsx
+++ src/components/moleculas/Tarjeta.jsx
@@ -3,17 +3,25 @@ import Parrafo from '../atomos/Parrafo/Parrafo'
 import Titulo from '../atomos/Titulo/Titulo'
 import Boton from '../atomos/Boton/Boton'
 import './Tarjeta.css'
+import { useState } from 'react'
 
 // eslint-disable-next-line react/prop-types
-function Tarjeta ({ img, nombre, descripcion }) {
+function Tarjeta ({ id, img, nombre, descripcion }) {
+
+  const [objeto, setObjeto] = useState([])
+
   const eventoClick = () => {
+
     const elementoGuardados = {
+      id,
       nombre,
       imagen: img,
       descripcion
     }
 
-    localStorage.setItem('personajes', JSON.stringify(elementoGuardados))
+    setObjeto(estadoAnterior =>({...elementoGuardados}))
+    console.log(objeto)
+
   }
 
   return (
